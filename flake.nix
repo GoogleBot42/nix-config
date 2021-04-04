933265347119836ce04a0b024bfb901554137bb7
zuckerberg <5-zuckerberg@users.noreply.git.neet.dev>
2021-04-03 20:09:21 -0400

    add mitty and nanachi

--- flake.nix
+++ flake.nix
@@ -8,6 +8,14 @@
         system = "x86_64-linux";
         modules = [ ./reg/configuration.nix ];
       };
+      "mitty" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./mitty/configuration.nix ];
+      };
+      "nanachi" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./nanachi/configuration.nix ];
+      };
       "neetdev" = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [ ./neet.dev/configuration.nix ];
