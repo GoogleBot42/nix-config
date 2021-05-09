f0205da719f0e461aaf680b9c3e959ab1dddf3b8
zuckerberg <5-zuckerberg@users.noreply.git.neet.dev>
2021-05-09 10:35:37 -0400

    don't use grub for compute modules

--- machines/compute/common.nix
+++ machines/compute/common.nix
@@ -5,6 +5,11 @@
     ../../common/common.nix
   ];
 
+  # NixOS wants to enable GRUB by default
+  boot.loader.grub.enable = false;
+  # Enables the generation of /boot/extlinux/extlinux.conf
+  boot.loader.generic-extlinux-compatible.enable = true;
+
   nix.flakes.enable = true;
 
   networking.interfaces.eth0.useDHCP = true;
