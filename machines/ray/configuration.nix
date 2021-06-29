6e2ba8663812f7a76561e7689bdd059e1a52a2b4
zuckerberg <5-zuckerberg@users.noreply.git.neet.dev>
2021-06-29 12:07:56 -0400

    try direct luks

--- machines/ray/configuration.nix
+++ machines/ray/configuration.nix
@@ -13,12 +13,17 @@
 
   efi.enable = true;
 
-  luks = {
-    enable = true;
-    device = {
-      path = "/dev/disk/by-uuid/fbe946d3-414f-4c2e-bb24-b845870fde6c";
-      allowDiscards = true;
-    };
+  # luks = {
+  #   enable = true;
+  #   device = {
+  #     path = "/dev/disk/by-uuid/fbe946d3-414f-4c2e-bb24-b845870fde6c";
+  #     allowDiscards = true;
+  #   };
+  # };
+
+  boot.initrd.luks.devices."enc-pv" = {
+    device = "/dev/disk/by-uuid/fbe946d3-414f-4c2e-bb24-b845870fde6c";
+    allowDiscards = true;
   };
 
   networking.hostName = "ray";
