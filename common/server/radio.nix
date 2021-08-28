8b780f0f1a0098519247f386c72ac9ab757dc55f
zuckerberg <5-zuckerberg@users.noreply.git.neet.dev>
2021-08-28 10:01:30 -0400

    update radio

--- common/server/radio.nix
+++ common/server/radio.nix
@@ -60,7 +60,8 @@ in {
       enable = true;
       after = ["network.target"];
       wantedBy = ["multi-user.target"];
-      serviceConfig.ExecStart = "${radioPackage}/bin/radio ${services.icecast.listen.address}:${services.icecast.listen.port} ${services.icecast.mount} 5500";
+      serviceConfig.ExecStart = "${radioPackage}/bin/radio 
+        ${config.services.icecast.listen.address}:${config.services.icecast.listen.port} ${config.services.icecast.mount} 5500";
       serviceConfig.User = cfg.user;
       serviceConfig.Group = cfg.group;
       serviceConfig.WorkingDirectory = cfg.dataDir;
