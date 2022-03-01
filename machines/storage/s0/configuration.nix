7feddbccebdbf32db5f3ba5d700dad7ac00cf4af
zuckerberg <5-zuckerberg@users.noreply.git.neet.dev>
2022-03-01 18:08:17 -0500

    Helios64 NAS

--- machines/storage/s0/configuration.nix
+++ machines/storage/s0/configuration.nix
@@ -2,11 +2,27 @@
 
 {
   imports =[
-    ./helios64.nix
+    ./helios64
   ];
 
+  # nsw2zwifzyl42mbhabayjo42b2kkq3wd3dqyl6efxsz6pvmgm5cup5ad.onion
+
   nix.flakes.enable = true;
+
   networking.hostName = "s0";
-  fileSystems."/" = { device = lib.mkForce "/dev/disk/by-label/bold-emmc"; fsType = lib.mkForce "btrfs"; };
+
+  luks = {
+    enable = true;
+    device = {
+      path = "/dev/disk/by-uuid/975d8427-2c6a-440d-a1d2-18dd15ba5bc2";
+      allowDiscards = true;
+    };
+  };
+
+  boot.loader.grub.enable = false;
+  boot.loader.generic-extlinux-compatible.enable = true;
+
   networking.interfaces.eth0.useDHCP = true;
+
+  zramSwap.enable = true;
 }
