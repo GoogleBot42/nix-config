5b084fffccb412530b87ee6180b1c31b01d7f2f1
Zuckerberg <zuckerberg@neet.dev>
2023-04-18 23:15:03 -0600

    moonlander

--- machines/ray/configuration.nix
+++ machines/ray/configuration.nix
@@ -18,10 +18,20 @@
   hardware.openrazer.devicesOffOnScreensaver = false;
   users.users.googlebot.packages = [ pkgs.polychromatic ];
 
-  # depthai
   services.udev.extraRules = ''
+    # depthai
     SUBSYSTEM=="usb", ATTRS{idVendor}=="03e7", MODE="0666"
+
+    # Moonlander
+    # Rules for Oryx web flashing and live training
+    KERNEL=="hidraw*", ATTRS{idVendor}=="16c0", MODE="0664", GROUP="plugdev"
+    KERNEL=="hidraw*", ATTRS{idVendor}=="3297", MODE="0664", GROUP="plugdev"
+    # Wally Flashing rules for the Moonlander and Planck EZ
+    SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="df11", MODE:="0666", SYMLINK+="stm32_dfu"
   '';
+  users.groups.plugdev = {
+    members = [ "googlebot" ];
+  };
 
   # virt-manager
   virtualisation.libvirtd.enable = true;
