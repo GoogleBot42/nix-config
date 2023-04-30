162b544249bbf6a2a62ccfb8c3763e1afa280e20
Zuckerberg <zuckerberg@neet.dev>
2023-04-30 09:13:49 -0600

    Set binary cache priority

--- common/binary-cache.nix
+++ common/binary-cache.nix
@@ -4,13 +4,13 @@
   nix = {
     settings = {
       substituters = [
-        "http://s0.koi-bebop.ts.net:5000"
-        "https://nix-community.cachix.org"
         "https://cache.nixos.org/"
+        "https://nix-community.cachix.org"
+        "http://s0.koi-bebop.ts.net:5000"
       ];
       trusted-public-keys = [
-        "s0.koi-bebop.ts.net:OjbzD86YjyJZpCp9RWaQKANaflcpKhtzBMNP8I2aPUU="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "s0.koi-bebop.ts.net:OjbzD86YjyJZpCp9RWaQKANaflcpKhtzBMNP8I2aPUU="
       ];
     };
   };
