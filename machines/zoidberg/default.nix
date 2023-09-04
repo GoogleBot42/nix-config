577e0d21bc92ae4f63fed76bebfd059037bff4e7
Zuckerberg <zuckerberg@neet.dev>
2023-09-03 21:58:08 -0600

    Xbox wireless controller support

--- machines/zoidberg/default.nix
+++ machines/zoidberg/default.nix
@@ -26,6 +26,12 @@
   services.xserver.desktopManager.retroarch.enable = true;
   services.xserver.desktopManager.retroarch.package = pkgs.retroarchFull;
 
+  # wireless xbox controller support
+  hardware.xone.enable = true;
+  boot.kernelModules = [ "xone-wired" "xone-dongle" ];
+  hardware.enableRedistributableFirmware = true;
+  hardware.enableAllFirmware = true;
+
   users.users.cris = {
     isNormalUser = true;
     hashedPassword = "$y$j9T$LMGwHVauFWAcAyWSSmcuS/$BQpDyjDHZZbvj54.ijvNb03tr7IgX9wcjYCuCxjSqf6";
