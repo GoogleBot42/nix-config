bdd2d9bef92e5b10e682acf82950fee43cbc8c94
Zuckerberg <zuckerberg@neet.dev>
2024-10-11 21:20:18 -0700

    Update nextcloud

--- common/server/nextcloud.nix
+++ common/server/nextcloud.nix
@@ -8,7 +8,7 @@ in
   config = lib.mkIf cfg.enable {
     services.nextcloud = {
       https = true;
-      package = pkgs.nextcloud29;
+      package = pkgs.nextcloud30;
       hostName = "neet.cloud";
       config.dbtype = "sqlite";
       config.adminuser = "jeremy";
