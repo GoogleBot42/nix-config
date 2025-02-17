b2acaff7831275ac75327bcf86d105883ec9096f
Zuckerberg <zuckerberg@neet.dev>
2025-02-17 12:09:20 -0800

    Fix pykms by downgrading to python 3.11

--- overlays/default.nix
+++ overlays/default.nix
@@ -6,4 +6,8 @@ let
 in
 {
   actual-server = prev.callPackage ./actualbudget { };
+
+  # Copied entire package from nixpkgs to downgrade to python 3.11 since 3.12 is broken.
+  # See: https://github.com/Py-KMS-Organization/py-kms/issues/117
+  pykms = prev.callPackage ./pykms.nix { };
 }
