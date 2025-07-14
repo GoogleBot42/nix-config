af9e462b27b839599ea3296be2d27655ac1bf0fe
Zuckerberg <zuckerberg@neet.dev>
2025-07-13 17:54:32 -0700

    Allow substituters to be offline

--- common/binary-cache.nix
+++ common/binary-cache.nix
@@ -12,6 +12,13 @@
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "s0.koi-bebop.ts.net:OjbzD86YjyJZpCp9RWaQKANaflcpKhtzBMNP8I2aPUU="
       ];
+
+      # Allow substituters to be offline
+      # This isn't exactly ideal since it would be best if I could set up a system
+      # so that it is an error if a derivation isn't available for any substituters
+      # and use this flag as intended for deciding if it should build missing
+      # derivations locally. See https://github.com/NixOS/nix/issues/6901
+      fallback = true;
     };
   };
 }
